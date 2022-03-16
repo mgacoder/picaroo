466b6c5c6b3dd614c75cf1c7e85d510ab1ded6e1
mgacoder <miguelgalvanalvarado@gmail.com>
2022-03-15 23:23:13 -0700

    added hbs files

--- application/routes/index.js
+++ application/routes/index.js
@@ -3,7 +3,22 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('./partials/index.hbs', {name: "Miguel"});
+  res.render('./partials/index.hbs', {name: "Miguel", age:35});
 });
 
+router.get('/login', (req, res, next) =>{
+  res.render('./partials/login');
+});
+
+router.get('/imagepost', (req, res, next) => {
+  res.render('./partials/imagepost');
+});
+
+router.get('/postimage', (req, res, next) => {
+  res.render('./partials/postimage');
+});
+
+router.get('/registration', (req, res, next) => {
+  res.render('./partials/registration');
+});
 module.exports = router;
