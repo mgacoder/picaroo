92cf68b3bc435c2fde9fcc0275e92c0efdd47109
mgacoder <miguelgalvanalvarado@gmail.com>
2022-03-15 21:05:03 -0700

    added handlebars to project

--- application/routes/index.js
+++ application/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.sendFile('index.html');
+  res.render('./partials/index.hbs', {name: "Miguel"});
 });
 
 module.exports = router;
